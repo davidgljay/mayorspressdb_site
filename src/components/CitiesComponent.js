2fe22699b17f79e98fa8a7af1f7ee4404348dacb
David Jay <davidgljay@gmail.com>
2016-01-28 20:04:17 -0500

    Adding key to array, re-adding app div to index.html.

--- src/components/CitiesComponent.js
+++ src/components/CitiesComponent.js
@@ -7,8 +7,8 @@ require('styles/Cities.scss');
 let CitiesComponent = (props) => (
   <div className="cities-component">
   	{props.cities.map(function(city) {
-  		var selected = city.code == props.selected ? 'selected':'';
-  		return <button title={city.full} className={selected + ' btn btn-default'}>{city.code}</button>
+  		var selected = city.key == props.selected ? 'selected':'';
+  		return <button title={city.full} className={selected + ' btn btn-default'}>{city.key}</button>
   	})}
   </div>
 );
@@ -19,16 +19,16 @@ CitiesComponent.displayName = 'CitiesComponent';
 // CitiesComponent.propTypes = {};
 CitiesComponent.defaultProps = {
 	cities:[
-		{code:'NYC', full:'New York City'},
-		{code:'LAX', full:'Los Angeles'},
-		{code:'CHI', full:'Chicago'},
-		{code:'IAH', full:'Houston'},
-		{code:'PHL', full:'Philadelphia'},
-		{code:'PHX', full:'Pheonix'},
-		{code:'SAT', full:'San Antonio'},
-		{code:'SAN', full:'San Diego'},
-		{code:'DFW', full:'Dallas'},
-		{code:'SJC', full:'San Jose'}
+		{key:'NYC', full:'New York City'},
+		{key:'LAX', full:'Los Angeles'},
+		{key:'CHI', full:'Chicago'},
+		{key:'IAH', full:'Houston'},
+		{key:'PHL', full:'Philadelphia'},
+		{key:'PHX', full:'Pheonix'},
+		{key:'SAT', full:'San Antonio'},
+		{key:'SAN', full:'San Diego'},
+		{key:'DFW', full:'Dallas'},
+		{key:'SJC', full:'San Jose'}
 	],
 	selected:'NYC'
 };
